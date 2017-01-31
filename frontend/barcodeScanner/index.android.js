74fcbcff39707920c8672d83a0fd49f0bf0e6a45
adoundakov <adoundakov@gmail.com>
2017-01-31 13:41:05 -0800

    add camera js

--- frontend/barcodeScanner/index.android.js
+++ frontend/barcodeScanner/index.android.js
@@ -1,10 +1,22 @@
-import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+// import React, { Component } from 'react';
+// import { AppRegistry, View } from 'react-native';
 
-import Header from './src/components/header';
+// import Header from './src/components/header';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native';
+import Camera from 'react-native-camera';
+
+import BadInstagramCloneApp from './src/components/camera';
 
 const App = () => (
-  <Header headerText={'Barcode Scanner'}/>
+  <BadInstagramCloneApp />
 );
 
 AppRegistry.registerComponent('barcodeScanner', () => App);
